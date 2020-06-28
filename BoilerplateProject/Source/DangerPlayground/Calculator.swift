febfed53a4d269a5468badf24e52e7318fbbfa66
mugbug <zaronipedro@outlook.com>
2020-06-28 12:10:19 -0300

    Create a calculator

---
+++ BoilerplateProject/Source/DangerPlayground/Calculator.swift
@@ -0,0 +1,13 @@
+//
+//  File.swift
+//  BoilerplateProject
+//
+//  Created by Pedro M. Zaroni on 28/06/20.
+//  Copyright © 2020 mugbug. All rights reserved.
+//
+
+struct Calculator {
+    static func sum(_ a: Int, with b: Int) -> Int {
+        return a + b
+    }
+}
