44902f2e087726f8991ceb6d0094ce94a6dc1e0d
mugbug <zaronipedro@outlook.com>
2020-06-28 10:56:40 -0300

    Add lint step to danger

--- BoilerplateProject/Source/Application/AppDelegate.swift
+++ BoilerplateProject/Source/Application/AppDelegate.swift
@@ -13,9 +13,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
     var appRouter: AppRouter?
 
-    func application(
-        _ application: UIApplication,
-        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
     ) -> Bool {
 
         #if DEBUG
